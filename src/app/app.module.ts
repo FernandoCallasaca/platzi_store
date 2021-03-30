5f9300dd3cc41abd57ca41ba69c298396004b796
Fernando Callasaca <fernandocallasaca@outlook.com>
2021-03-30 14:06:24 -0500

    Agregamos swiper modificamos el angular.json especificamente el styles modificamos el hijo banner y luego ya con AfterViewInit modificamos el padre home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,9 @@ import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   // En declaraciones, pipes, directivas colocamos el componente
@@ -33,7 +36,10 @@ import { DemoComponent } from './demo/demo.component';
     HomeComponent,
     ProductsComponent,
     ContactComponent,
-    DemoComponent
+    DemoComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+// 3.- Luego de importar Swiper y crear la variable
+// Incluimos el elemento AfterViewInit que nos dice cuando los elementos hijos ya fueron renderizados
+// O sea ya debío renderizar las imágenes y el html de banner.component.html
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+// 1.- importamos Swiper de swiper
+// Para eso tuvimos que modificar el angular.json el styles de
+// Lo ponemos aquí porque es la base de todo el banner o sea este es el home
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+// 4.- Luego implementamos el AfterViewInit
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  // 2.- Creamos una variable mySwiper de tipo Swiper
+  mySwiper: Swiper;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // 5.- Y ahí colocamos ese método ya que sus hijos o el banner.component.html ya está renderizado
+  ngAfterViewInit(): void {
+    // 6.- Y le damos una clase para identificar y esa clase está en el html del banner.component.html
+    this.mySwiper = new Swiper('.swiper-container', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      }});
+  }
 }
