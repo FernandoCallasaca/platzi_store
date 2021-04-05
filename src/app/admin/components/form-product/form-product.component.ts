63963715f9739bfeedadf4b3cc2e6c4139c6a697
Fernando Callasaca <fernandocallasaca@outlook.com>
2021-04-05 09:12:31 -0500

    agregando metodo saveProducto y llamamos en el form al (ngSubmit) event para preventDefault y router.navigate

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -3,6 +3,10 @@ import { Component, OnInit } from '@angular/core';
 // FormGroup = grupo de controler, muchos controles
 // Validators para empezar a poner las validaciones a cada FormControl dentro del FormBuilder
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProductsService } from './../../../core/services/products/products.service';
+// Importamos para redireccionar y este es una injección de dependencia
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-form-product',
   templateUrl: './form-product.component.html',
@@ -13,7 +17,9 @@ export class FormProductComponent implements OnInit {
   form: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private productsService: ProductsService,
+    private router: Router
   ) {
     // Como el builForm es una contrucción lo llamamos en el constructor
     // no podemos llmar en el ngOnInit porque eso es para llamar datos
@@ -21,6 +27,17 @@ export class FormProductComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  saveProduct(event: Event): void {
+    event.preventDefault();
+    if (this.form.valid) {
+      const product = this.form.value;
+      this.productsService.createProduct(product).subscribe(newProduct => {
+        console.log(newProduct);
+        this.router.navigate(['./admin/products']);
+      });
+    }
   }
 
   private buildForm(): void {
