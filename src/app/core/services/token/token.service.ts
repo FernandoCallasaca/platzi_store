f8e58569bfdcdc64cef6f0da81a96cd2d94c2b08
Fernando Callasaca <fernandocallasaca@outlook.com>
2021-06-11 12:32:20 -0500

    agregando comentarios al servicio token

--- src/app/core/services/token/token.service.ts
+++ src/app/core/services/token/token.service.ts
@@ -8,10 +8,13 @@ export class TokenService {
   constructor() { }
 
   saveToken(token: string): void {
+    // una forma de guardar el token es en el localStorage
+    // la mejor forma de guardarlo es el cookies
     localStorage.setItem('token', token);
   }
 
   getToken(): string {
+    // de esta forma sacamos el token del localStorage
     return localStorage.getItem('token');
   }
 }
